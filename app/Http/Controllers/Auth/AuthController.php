af486295e3a7cbfa83d1aae4081d0bbe8e944a0c
Maxime Maheo <imaxame@gmail.com>
2016-08-19 16:53:00 +0200

    translate login and reset password pages

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -29,6 +29,7 @@ class AuthController extends Controller
      * @var string
      */
     protected $redirectTo = '/';
+    protected $redirectPath = '/';
 
     /**
      * Create a new authentication controller instance.
--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Cliquez <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> ici </a> pour réinitialiser votre mot de passe
