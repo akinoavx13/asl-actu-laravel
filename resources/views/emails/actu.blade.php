0e9c0f820dce80cfb39bcd63f29b2d0b879d7ddf
Maxime Maheo <imaxame@gmail.com>
2017-09-15 17:03:32 +0200

    name in bold

--- resources/views/emails/actu.blade.php
+++ resources/views/emails/actu.blade.php
@@ -8,7 +8,7 @@
     <p>Bonjour, {{ $user['forname'] }} {{ $user['name'] }}</p>
     <br>
 
-    <p>Une nouvelle actualité vient d'être écrite par {{ $writer->forname }} {{ $writer->name }} dans la catégorie <span style="font-weight: bold">{{ $categoryName }}</span>. Merci de ne pas répondre à ce mail mais
+    <p>Une nouvelle actualité vient d'être écrite par <b>{{ $writer->forname }} {{ $writer->name }}</b> dans la catégorie <span style="font-weight: bold">{{ $categoryName }}</span>. Merci de ne pas répondre à ce mail mais
         directement sur <a href="http://actu.aslectra.com">ACTU ASLectra</a> !</p>
 
     <p>Actualité : </p>
